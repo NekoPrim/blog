b37cb5dd2284f9d9f34a9040fbd6ece424b0a123
Desiree Quade <desiree.quade@kyros.care>
2022-06-05 19:11:13 -0500

    added reducer

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+
+    const { data, addBlogPosts } = useContext(BlogContext);
+
     return(
         <View>
             <Text>
                 Index Screen
             </Text>
+            <Button 
+                title="Add Post"
+                onPress={addBlogPosts}
+            />
+            <FlatList
+                data={data}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) => {
+                    return <Text>{item.title}</Text>;
+                }}
+            />
         </View>
     );
 };
